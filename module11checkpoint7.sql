5d22b84ba34e7b2bfbf3d5de4a073c0d0966714d
87designer <41029825+87designer@users.noreply.github.com>
2019-08-04 21:25:53 -0400

    Create module11checkpoint7.sql

---
+++ module11checkpoint7.sql
@@ -0,0 +1,16 @@
+-- Using sfo_calendar, sfo_listings, and sfo_reviews answer the following questions.
+
+-- Q1
+-- What's the most expensive listing?
+-- What else can you tell me about the listing?
+
+
+
+-- Q2
+-- What neighborhoods seem to be the most popular?
+
+
+
+-- Q3
+-- What time of year is the cheapest time to go to San Francisco?
+-- What about the busiest?
